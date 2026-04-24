d818c4931adcaa8c31857464e3aa2c452be8346d
ramontrivia <ramon@triviasistem.com>
2026-04-24 17:41:49 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ app.get("/", (req, res) => {
   res.status(200).send("OK");
 });
 
+// 🔥 IMPORTANTE: TODAS AS REQUISIÇÕES PASSAM PELO WEBHOOK
 app.use("/", webhookRoutes);
 
 app.listen(PORT, () => {

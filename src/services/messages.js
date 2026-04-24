0982cfbc755a6e41cfb09669a07f4d97eed11473
ramontrivia <ramon@triviasistem.com>
2026-04-24 18:45:58 -0300

    Update messages.js

--- src/services/messages.js
+++ src/services/messages.js
@@ -19,6 +19,7 @@ export async function saveMessage({ company, from, content, role }) {
         ditection: role,
         message: content,
         intent: null,
+        content: content,
         role: role
       }
     ])

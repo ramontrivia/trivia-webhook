7aa09380f6e08bd975869799d35fa1c9f63e1dff
ramontrivia <ramon@triviasistem.com>
2026-04-24 16:29:47 -0300

    Update messages.js

--- src/services/messages.js
+++ src/services/messages.js
@@ -1,1 +1,23 @@
+import { supabase } from "./supabase.js";
 
+export async function saveMessage({
+  companyId,
+  from,
+  content,
+  role
+}) {
+  const { error } = await supabase
+    .from("messages")
+    .insert([
+      {
+        company_id: companyId,
+        from_number: from,
+        content,
+        role
+      }
+    ]);
+
+  if (error) {
+    console.error("Erro ao salvar mensagem:", error.message);
+  }
+}

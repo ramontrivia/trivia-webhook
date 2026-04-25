ee21ab90b1e5efca244ec17466a73d26455b52e7
ramontrivia <ramon@triviasistem.com>
2026-04-25 08:48:13 -0300

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,1 +1,19 @@
+import express from "express";
 
+const router = express.Router();
+
+router.get("/admin", async (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Painel TRIVIA</title>
+      </head>
+      <body>
+        <h1>Painel TRIVIA</h1>
+        <p>Painel carregado com sucesso.</p>
+      </body>
+    </html>
+  `);
+});
+
+export default router;

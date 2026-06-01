06b5970fe06fbd1d3794c678610f9d01570903ec
ramontrivia <ramon@triviasistem.com>
2026-06-01 20:56:31 -0300

    Update orchestrator.js

--- src/services/orchestrator.js
+++ src/services/orchestrator.js
@@ -230,7 +230,7 @@ async function sendInstagramMessage({ to, message }) {
     }
 
     await axios.post(
-      `https://graph.facebook.com/${GRAPH_VERSION}/me/messages`,
+      `https://graph.facebook.com/${GRAPH_VERSION}/17841402938162053/messages`,
       {
         recipient: { id: to },
         message:   { text: message },
